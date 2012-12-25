f579a9aafbc65380e47eaedea028057b938bb330
Anton Romanovich <anthony.romanovich@gmail.com>
2012-12-25 18:04:00 +0600

    Fix Jinja2 and gettext integration; don't use `PROJECT_DIR`

--- build.py
+++ build.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 import glob
 import shutil
@@ -8,8 +9,6 @@ import subprocess
 import markdown
 from jinja2 import Environment, FileSystemLoader, contextfunction
 
-
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DEFAULT_PAGE = 'about'
 DEFAULT_LANGUAGE = 'ru'
@@ -33,17 +32,13 @@ def url_for(context, name, language=None):
 def create_jinja2_environment(language):
     env = Environment(loader=FileSystemLoader('layouts'),
                       extensions=['jinja2.ext.i18n'])
-
+    
     if language == 'ru':
         translations = gettext.GNUTranslations(open('messages.mo'))
         env.install_gettext_translations(translations, newstyle=True)
-        # Hack:
-        globals_gettext = env.globals['gettext']
-        env.globals['gettext'] = contextfunction(
-            lambda context, s: unicode(globals_gettext(context, s).decode('utf-8')))
     elif language == 'en':
         env.install_null_translations(newstyle=False)
-
+    
     env.globals.update(url_for=url_for, LANGUAGE=language)
     return env
 
@@ -52,10 +47,10 @@ def render_template(template, context, target_fname):
     target_dir = os.path.dirname(target_fname)
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
-
+    
     render = template.render(**context)
     with codecs.open(target_fname, 'w', 'utf-8') as f:
-        f.write(render)
+        f.write(render) 
 
 
 def build(build_dir):
@@ -76,14 +71,14 @@ def build(build_dir):
             template = env.get_template(layout)
 
             context = {
-                'page_name': page,
+                'page_name': page,        
             }
-            page_dir = os.path.join(PROJECT_DIR, 'pages', page, language)
+            page_dir = os.path.join('pages', page, language)
             for fname in glob.glob('%s/*.md' % page_dir):
                 block_name, ext = os.path.splitext(os.path.basename(fname))
                 with codecs.open(fname, 'r', 'utf-8') as f:
                     context[block_name] = markdown.markdown(f.read())
 
             target_fname = os.path.join(
-                PROJECT_DIR, build_dir, path_for(page, language=language), 'index.html')
+                build_dir, path_for(page, language=language), 'index.html')
             render_template(template, context, target_fname)
