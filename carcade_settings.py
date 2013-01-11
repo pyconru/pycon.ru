291e36c54e609a4b6ce1aa67cfef2e0eed69ff9d
Anton Romanovich <anthony.romanovich@gmail.com>
2013-01-12 01:50:52 +0600

    Fixes

--- carcade_settings.py
+++ carcade_settings.py
@@ -1,3 +1,6 @@
+from webassets import Bundle
+
+
 LANGUAGES = ('en', 'ru')
 
 DEFAULT_PAGE = 'about'
@@ -7,4 +10,10 @@ DEFAULT_LAYOUT = 'page.html'
 LAYOUTS = {
     'about': 'index.html',
     'register': 'register.html',
+}
+
+BUNDLES = {
+    'css': Bundle('./css/reset.css', './css/normalize.css', 
+                  Bundle('./less/styles.less', filters='less'),
+                  output='./gen/styles.css'),
 }
