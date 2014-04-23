8448579f6f7e38f16a4e499b60d7efabcbef003d
elenaitpeople <elena@it-people.ru>
2014-04-23 17:51:19 +0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -39,5 +39,10 @@ ORDERING = {
         'writing-secure-apis',
         'the-sorry-state-of-ssl',
         'explore-your-data',
+        'python_in_yandex',
+        'pony_orm',
+        'open_source_advices',
+        'python_pytest',
+        'soa',
     ],
 }
